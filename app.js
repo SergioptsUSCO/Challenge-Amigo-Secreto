c80cce667ec18c4e9d3f757628923ad17a27545d
Sergio Puentes <sergioandrespuentescicery@gmail.com>
2025-08-05 17:39:10 -0500

    actualizacion de la logica

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-//El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombreAmigos = [];
+
+function agregarAmigo(){
+    let nombre = document.getElementById("amigo").value;
+    if (nombre.trim() === "") {
+        alert("Por favor, ingresa un nombre");
+    } else {
+        nombreAmigos.push(nombre);
+        actualizarLista();
+    }
+    limpiarInput();
+}
+
+function limpiarInput() {
+    let limpiar = document.querySelector('#amigo').value = '';
+    return;
+}
+
+function actualizarLista() {
+    let listaElementos = document.getElementById("listaAmigos");
+    listaElementos.innerHTML = ""; 
+    for (let i = 0; i < nombreAmigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = nombreAmigos[i];
+        listaElementos.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (nombreAmigos.length < 2) {
+        alert("Por favor, agrega mas nombres de amigos.");
+        return;
+    }
+    let listaElementos = document.getElementById("listaAmigos");
+    listaElementos.innerHTML = "";
+    let indice = Math.floor(Math.random() * nombreAmigos.length);
+    let li = document.createElement("li");
+        li.textContent = nombreAmigos[indice];
+        listaElementos.appendChild(li);
+}
+
