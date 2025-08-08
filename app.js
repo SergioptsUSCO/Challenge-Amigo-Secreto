b1589ddf0becf357a9feee464bf4336b77483389
Sergio Puentes <u20242228195@usco.edu.co>
2025-08-08 08:28:49 -0500

    Update app.js

--- app.js
+++ app.js
@@ -35,9 +35,10 @@ function sortearAmigo() {
     listaElementos.innerHTML = "";
     let indice = Math.floor(Math.random() * nombreAmigos.length);
     let li = document.createElement("li");
-        li.textContent = "Tu amigo secreto es: ", nombreAmigos[indice];
+        li.textContent = `Tu amigo secreto es:  ${nombreAmigos[indice]}`;
         listaElementos.appendChild(li);
 }
+
 
 
 
